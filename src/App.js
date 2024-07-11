5ac7904fc6d6b34fce721e7465fddeb7237a4ca2
Sarfraz Ahmad <sarfraz01010@gmail.com>
2024-07-11 19:06:48 +0500

    commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import './App.css'
+import { useState } from "react";
+
 
 function App() {
+  const [counter, setcounter] = useState(0);
+  useEffect(()=>{
+    alert('this page is going to reload!')
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={()=> setcounter((prevcount)=>prevcount - 1)}> - </button>
+    <h1>{counter}</h1>
+    <button onClick={()=>setcounter((prevcount) =>prevcount + 1)}> + </button>
+
+
     </div>
   );
 }
